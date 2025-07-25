d4068b3b2abb11c5bc16b93132414017f1352ba8
Jattzz <108416182+jattzz@users.noreply.github.com>
2025-07-25 23:22:58 +0530

    Create creating_strings.cpp

---
+++ creating_strings.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int> 
+#define vll vector<long long>
+#define pii pair<int, int> 
+#define pic pair<int, char> 
+#define mii map<int, int> 
+#define mll map<long long, long long>
+#define pb push_back
+#define mp make_pair
+#define sortv(v) sort(v.begin(), v.end())
+#define sumup(v) accumulate(v.begin(), v.end(), 0)
+#define ull unsigned long long int
+#define bs binary_search
+#define lb lower_bound
+#define ub upper_bound
+#define cinv for(auto &i: v) cin>>i
+ll power(ll x, ull y){
+    if(y==0){
+        return 1;
+    }
+    ll temp = power(x, y/2);
+    if(y%2==0){
+        return temp*temp;
+    }
+    else{
+        return x*temp*temp;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s; cin>>s;
+    sort(s.begin(), s.end());
+    vector<string> v;
+    ll cnt = 0;
+    do{
+        cnt++;
+        v.pb(s);
+    }while(next_permutation(s.begin(), s.end()));
+    cout<<cnt<<endl;
+    for(auto i: v) cout<<i<<endl;
+}
+
+
+// pretty standard question, just sort the string and then store all the permutations in a vector.
